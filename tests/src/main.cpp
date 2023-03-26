82019c75a5bc32fe86df12c17a6ec53e2da5c5fb
ArchLeaders <archparliament@gmail.com>
2023-03-26 01:16:47 -0700

    Update test (bfwav)

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <nw/snd/bfwav.h>
+#include <utils/file_util.h>
 
-int main() {
+int main(int argc, char** argv) {
   std::cout << "[c++] Init Testing" << std::endl;
+  const auto file = file::util::ReadAllBytes(argv[1]);
+  const auto fwav = nw::snd::Fwav::FromBinary({file.data(), file.size()});
+
+  std::ofstream stream(std::string(argv[1]) + ".data");
+  const auto data = fwav.data;
+  stream.write(reinterpret_cast<const char*>(data.begin()), data.size());
+
+  for (size_t i = 0; i < fwav.channels.size(); i++) {
+    std::cout << fwav.channels[0].adpcm_entry.flag << std::endl;
+    std::cout << fwav.channels[0].sound_data_entry.flag << std::endl;
+  }
 }
